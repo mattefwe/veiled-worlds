1ca7a2756b433fbcc017f02b9301158b23af96ab
mattefwe <matterisco@gmail.com>
2026-01-30 23:02:05 +0100

    feat: added alien list template

---
+++ veiled-worlds-game/scripts/autoload/alien_list.gd
@@ -0,0 +1,59 @@
+extends Node
+
+var deficenti =[
+	{
+		"id" : "N",
+		"picture" : "Path",
+		"name" : "Name",
+		"hostile" : "Boolean",
+		
+		"planetName" : "PlanetName",
+		
+		"atmosphere" : "Value",
+		"gravity" : "Value",
+		"water" : "Value",
+		"temperature" : "Value",
+		"resources" : "Value",
+		 
+		"ansAtm" : "Text",
+		"ansGvt" : "Text",
+		"ansWtr" : "Text",
+		"ansTmp" : "Text",
+		"ansRes" : "Text",
+		
+		"maxAnswers" : "Int",
+		
+		"extraDialogues" : [
+			{
+				"id" : "N.M",
+				"prerequisite" : "AtmAnswered",
+				"question" : "Text",
+				"answer" : "Text"
+			},
+			{
+				"id" : "N.M",
+				"prerequisite" : "GvtAnswered",
+				"question" : "Text",
+				"answer" : "Text"
+			},
+			{
+				"id" : "N.M",
+				"prerequisite" : "WtrAnswered",
+				"question" : "Text",
+				"answer" : "Text"
+			},
+			{
+				"id" : "N.M",
+				"prerequisite" : "TmpAnswered",
+				"question" : "Text",
+				"answer" : "Text"
+			},
+			{
+				"id" : "N.M",
+				"prerequisite" : "ResAnswered",
+				"question" : "Text",
+				"answer" : "Text"
+			},
+		]
+	}
+]
