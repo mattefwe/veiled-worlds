abe6cf360dc8fa23db6311518d4fee4977f1b547
private101st <matteo.albertinioffeddu@gmail.com>
2026-01-31 10:02:41 +0100

    feat: began changing way to update possible questions

--- veiled-worlds-game/scripts/instantiate_questions.gd
+++ veiled-worlds-game/scripts/instantiate_questions.gd
@@ -7,9 +7,14 @@ var GvtAnswered = false
 var WtrAnswered = false
 var TmpAnswered = false
 var ResAnswered = false
+var arrayquestions = []
 
-func _ready() -> void:
-	pass
+func _ready():
+	
+	for i in range(count_questions(currentid, arrayquestions)):
+		var label = Label.new()
+		label.text = arrayquestions[i]
+		add_child(label)
 
 func _process(_delta):
 	
@@ -19,41 +24,36 @@ func _process(_delta):
 		children[x].queue_free()
 	
 	## conto e riinizializzo tutti i figli
-	var arrayanswers = []
-	for i in range(count_questions(currentid, arrayanswers)):
-		var label = Label.new()
-		label.text = arrayanswers[i]
-		add_child(label)
 	
-func count_questions(id, arrayanswers):
+func count_questions(id, arrayquestions):
 	var alien = AlienList.aliens[id]
 	var extradialogues = alien.extraDialogues
-	arrayanswers.append("PlaceholderAtm")
-	arrayanswers.append("PlaceholderGvt") 
-	arrayanswers.append("PlaceholderWtr") 
-	arrayanswers.append("PlaceholderTmp")
-	arrayanswers.append("PlaceholderRes") 
+	arrayquestions.append("PlaceholderAtm")
+	arrayquestions.append("PlaceholderGvt") 
+	arrayquestions.append("PlaceholderWtr") 
+	arrayquestions.append("PlaceholderTmp")
+	arrayquestions.append("PlaceholderRes") 
 	var count = 0
 	for x in range(extradialogues.size()-1):
 		if(extradialogues[x].prerequisite == "AtmAnswered" and AtmAnswered == true):
 			count = count+1
-			arrayanswers.append(extradialogues[x].question)
+			arrayquestions.append(extradialogues[x].question)
 			
 		if(extradialogues[x].prerequisite == "GvtAnswered" and GvtAnswered == true):
 			count = count+1
-			arrayanswers.append(extradialogues[x].question)
+			arrayquestions.append(extradialogues[x].question)
 			
 		if(extradialogues[x].prerequisite == "WtrAnswered" and WtrAnswered == true):
 			count = count+1
-			arrayanswers.append(extradialogues[x].question)
+			arrayquestions.append(extradialogues[x].question)
 			
 		if(extradialogues[x].prerequisite == "TmpAnswered" and TmpAnswered == true):
 			count = count+1
-			arrayanswers.append(extradialogues[x].question)
+			arrayquestions.append(extradialogues[x].question)
 			
 		if(extradialogues[x].prerequisite == "ResAnswered" and ResAnswered == true):
 			count = count+1
-			arrayanswers.append(extradialogues[x].question)
+			arrayquestions.append(extradialogues[x].question)
 		
 		
 	return count
